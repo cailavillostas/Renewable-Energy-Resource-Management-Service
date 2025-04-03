572518de72fd65910229584907fe98e69da0a60e
Elijapearl <elipearlparafina@gmail.com>
2025-04-03 12:58:34 +0800

    final

--- router.js
+++ router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const energyController = require("./controller");
 
+
+
 router.get("/alerts/:id/usage", energyController.getEnergyAlert);
 
 module.exports = router;
